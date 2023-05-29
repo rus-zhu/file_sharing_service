792bafd8b87e38f5fd19ef9e53421f0eb8c2491f
rus-zhu <microlab982@yandex.ru>
2023-05-29 14:59:57 +0500

    init

---
+++ src/main/java/ru/ruszhu/file_sharing_service/FileSharingServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.ruszhu.file_sharing_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSharingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileSharingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ruszhu/file_sharing_service/FileSharingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ruszhu.file_sharing_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSharingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
